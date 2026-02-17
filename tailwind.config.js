3a22c69657f3d2be95722729867586584995727e
Pretheesh Thomas <me@bigmints.com>
2026-02-17 12:34:22 +0400

    factory: add feature Agent Core to ubot-core

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./public/**/*.html",
-    "./src/**/*.{ts,js}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
